eb8332b155fa0b1c2420923b20ec5e59dba102d3
Igor Bedesqui <igorbedesqui@gmail.com>
2023-11-09 03:40:06 +0000

    feat: demo new font utilities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>hej do</div>
-      <div className="font-wave text-7xl">hej do</div>
+      <div className="font-wave text-7xl font-normal">hej do</div>
       <div className="font-line text-7xl">hej do</div>
     </main>
   );
