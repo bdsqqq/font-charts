17c91a699f7944e9c0e3f92892ce936a426d44a9
Igor Bedesqui <igorbedesqui@gmail.com>
2023-11-09 03:39:40 +0000

    feat: surface font classes and variables through plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,39 @@
 import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
+const waveAndLineFontVariablesAndUtilities = plugin(function ({
+  theme,
+  matchUtilities,
+  addUtilities,
+}) {
+  matchUtilities(
+    {
+      font: (value) => {
+        return {
+          "--variable-font-wght": value,
+        };
+      },
+    },
+    {
+      values: theme("fontWeight"),
+      respectImportant: true,
+      supportsNegativeValues: false,
+    }
+  );
+
+  // when using font, surface the variable font settings through css variables
+  addUtilities({
+    ".font-wave": {
+      "font-family": "var(--font-wavefont)",
+      "font-variation-settings": `'wght' var(--variable-font-wght, 100), 'ROND' var(--variable-font-rond, 75), 'YELA' var(--variable-font-align, 0)`,
+    },
+    ".font-line": {
+      "font-family": "var(--font-linefont)",
+      "font-variation-settings": `'wght' var(--variable-font-wght, 100)`,
+    },
+  });
+});
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,12 +47,8 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily: {
-        wave: "var(--font-wavefont)",
-        line: "var(--font-linefont)",
-      },
     },
   },
-  plugins: [],
+  plugins: [waveAndLineFontVariablesAndUtilities],
 };
 export default config;
